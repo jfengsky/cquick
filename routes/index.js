118ae9b544bb90589f02a44bcdc750d8f3b24927
jfeng <jfengsky@gmail.com>
2014-03-17 11:47:04 +0800

    cquick modify

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
-
-/*
- * GET home page.
- */
-
-//exports.index = function(req, res){
-//  res.render('index', { title: 'Express' });
-//};
+var fs = require('fs'),
+  url = require('url');
 
 module.exports = function(app){
+
+  var walk = function(dir, done) {
+    var results = [];
+    var fileItem = {};
+    fs.readdir(dir, function(err, list) {
+      if (err) return done(err);
+      var i = 0;
+      (function next() {
+        var file = list[i++],
+          filename = file;
+        if (!file) return done(null, results);
+        file = dir + '/' + file;
+        fs.stat(file, function(err, stat) {
+          fileItem = filename + '|'+ file + '|' + stat.isDirectory() + '|' + stat.size + '|' + new Date(stat.mtime).getTime();
+          results.push(fileItem);
+          next();
+        });
+      })();
+    });
+  };
+
+
+
   app.get('/', function(req, res){
+    var path = req.query.path;
+    console.log(path);
     res.render('index', { title: 'Express' });
   });
 }
