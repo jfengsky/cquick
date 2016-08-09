c3339bc28e50bbaaecc9d1886bb0b8ef4f34dcfa
姜峰 <f@ctrip.com>
2016-08-09 14:14:00 +0800

    modify

--- useragent/test.js
+++ useragent/test.js
@@ -1,6 +1,27 @@
 var str1 = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7'
 var str2 = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
 var str3 = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0'
+var str4 = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586'
+var str5 = 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko'
+var str6 = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)'
+var str7 = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)'
+var str8 = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)'
+var str9 = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)'
+// console.log( str4.match(/(Chrome\/([\w+.])+|Safari\/([\w+.])+|Firefox\/([\w+.])+)/) )
+console.log(navigator.userAgent)
+var userAgent = navigator.userAgent
+var agentArray
+var browerType
+if(/(Trident|MSIE|Edge)/.test(userAgent)){
+  if(agentArray = userAgent.match(/MSIE\s\d+/)){
+    browerType = agentArray[0]
+  } else if( agentArray = userAgent.match(/Edge\/\d+/) ) {
+    browerType = agentArray[0]
+  } else {
+    browerType = userAgent.match(/rv:\d+/)[0] === 'rv:11' ? 'MSIE11' : 'MSIE11+'
+  }
+} else {
 
-console.log( str3.match(/(Chrome\/\d+|Safari\/\d+|Firefox\/\d+)/) )
-alert(navigator.userAgent)
+}
+
+console.log(browerType)
