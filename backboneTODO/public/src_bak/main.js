2c80e3b2bb6c55da820074974f6a2fb187aadeb3
jfengsky <jfengsky@gmail.com>
2016-08-26 14:13:26 +0800

    cloe modify

---
+++ backboneTODO/public/src_bak/main.js
@@ -0,0 +1,35 @@
+/**
+ * 主入口
+ */
+
+// 全局变量
+import GV from './gv'
+
+// 头部模块
+import Header from './header'
+
+// 任务列表区模块
+import TaskList from './taskList'
+
+// 底部区块
+import Footer from './footer'
+
+import { FETCH_LIST } from './fetch'
+
+$(document).ready(()=>{
+    // 从服务器获取task数据
+    FETCH_LIST()
+    .catch()
+    .then( data => {
+        GV.list = data
+
+        // 初始头部区块
+        Header.init()
+
+        // 初始化列表区块
+        TaskList.init()
+
+        // 初始化页脚区块
+        Footer.init()
+    })
+})
