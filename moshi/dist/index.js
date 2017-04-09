3e6f5f95906723d42844e0aeba9c1f7d342722cc
姜峰 <jiang.f@Ctrip.com>
2017-04-09 13:55:09 +0800

    moshi modify

--- moshi/dist/index.js
+++ moshi/dist/index.js
@@ -2,7 +2,41 @@ webpackJsonp([0],[
 /* 0 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: SyntaxError: Unexpected token (5:4)\n\n\u001b[0m \u001b[90m 3 | \u001b[39m\n \u001b[90m 4 | \u001b[39m\u001b[33mReactDOM\u001b[39m\u001b[33m.\u001b[39mrender(\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 5 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m   | \u001b[39m    \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 6 | \u001b[39m        webpack first\n \u001b[90m 7 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\u001b[33m,\u001b[39m\n \u001b[90m 8 | \u001b[39m    document\u001b[33m.\u001b[39mgetElementById(\u001b[32m'root'\u001b[39m)\u001b[0m\n");
+var payA = function () {};
+payA.prototype.calculate = function (pay) {
+    return pay * 4;
+};
+
+var payB = function () {};
+payB.prototype.calculate = function (pay) {
+    return pay * 3;
+};
+
+var payC = function () {};
+payC.prototype.calculate = function (pay) {
+    return pay * 2;
+};
+
+var Bouns = function () {
+    this.pay = null;
+    this.level = null;
+};
+Bouns.prototype.setPay = function (pay) {
+    this.pay = pay;
+};
+Bouns.prototype.setLevel = function (level) {
+    this.level = level;
+};
+
+Bouns.prototype.getPay = function () {
+    return this.level.calculate(this.pay);
+};
+
+var bouns = new Bouns();
+
+bouns.setPay(400);
+bouns.setLevel(new payA());
+console.log(bouns.getPay());
 
 /***/ })
 ],[0]);
--- moshi/src/index.js
+++ moshi/src/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+var payA = function(){}
+payA.prototype.calculate = function(pay){
+    return pay * 4
+}
 
-ReactDOM.render(
-    <div>
-        webpack first
-    </div>,
-    document.getElementById('root')
-)
+var payB = function(){}
+payB.prototype.calculate = function(pay){
+    return pay * 3
+}
+
+var payC = function(){}
+payC.prototype.calculate = function(pay){
+    return pay * 2
+}
+
+var Bouns = function(){
+    this.pay = null
+    this.level = null
+}
+Bouns.prototype.setPay = function(pay){
+    this.pay = pay
+}
+Bouns.prototype.setLevel = function(level){
+    this.level = level
+}
+
+Bouns.prototype.getPay = function(){
+    return this.level.calculate(this.pay)
+}
+
+var bouns = new Bouns()
+
+bouns.setPay(400)
+bouns.setLevel(new payA())
+console.log(bouns.getPay())
