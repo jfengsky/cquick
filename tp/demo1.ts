5e64cc11d58a2f69d1b8066969e465460bb94af6
姜峰 <jiang.f@Ctrip.com>
2017-05-06 21:06:15 +0800

    save

---
+++ tp/demo1.ts
@@ -0,0 +1,10 @@
+const a:String = ``
+class Person {
+  get name(){
+    return name
+  }
+}
+
+const add:Function = (a, b) => a + b
+
+// const list: Array<Number,Object> = []
---
+++ tp/js_output/demo1.js
@@ -0,0 +1,7 @@
+const a = ``;
+class Person {
+    get name() {
+        return name;
+    }
+}
+const add = (a, b) => a + b;
