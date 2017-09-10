2050f573dea9978194068523828b4e4b76e7c27a
jfengsky <jfengsky@gmail.com>
2017-09-10 22:49:25 +0800

    save

---
+++ webpack-quick-start/build/webpack.config.base.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack')
+module.exports = {
+    entry: {
+        index: '../src/index.js'
+    },
+    output: {
+        path: __dirname + '/dist/',
+        filename: [name].js
+    }
+}
